b35fe3517a1b6b936a199263dac0a2137d80ae3f
Igork <igor.kryt@mail.ru>
2024-01-20 19:55:37 +0400

    fix

--- ChronoFlow/ChronoFlow.API/Modules/EventsModule/EventAnalyticsModule.cs
+++ ChronoFlow/ChronoFlow.API/Modules/EventsModule/EventAnalyticsModule.cs
@@ -5,8 +5,6 @@
         public string Name { get; set; }
         public TimeSpan Time { get; set; }
         public int Count { get; set; }
-
-        public int TotalCount { get; set; }
         
         public EventAnalyticsModule(string name, TimeSpan time, int count)
         {
